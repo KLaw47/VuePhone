38d11773db803b9a77caa64c4c6366ed6775d51c
Keaton Law <keaton.c.law@gmail.com>
2023-06-15 10:47:25 -0500

    initial

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue'
+import ContactForm from './components/ContactForm.vue'
+import ContactList from './components/ContactList.vue'
+import sampleData from './sampleData'
+
+const contacts = ref(JSON.parse(localStorage.getItem('contacts')))
+
+const setContactsInLocalStorage = () => {
+  localStorage.setItem('contacts', JSON.stringify(contacts.value))
+}
+
+const addContact = (contact) => {
+  contacts.value = [...contacts.value, contact]
+  setContactsInLocalStorage()
+}
+
+const deleteContact = (contact) => {
+  const index = contacts.value.indexOf(contact)
+  if (index !== -1) {
+    contacts.value.splice(index, 1)
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1 class="is-size-1 has-text-centered has-text-info">Vue Phonebook</h1>
+    <div class="container px-4">
+      <div class="columns">
+        <div class="column">
+          <ContactForm @submit="addContact" />
+        </div>
+        <div class="column">
+          <ContactList :contacts="contacts" @remove="deleteContact"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
